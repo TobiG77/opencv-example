8e70292fa5b4e080c0716e53fc782e2c8d1b5b77
Tobias Gerschner <tobias@deepthought>
2017-11-09 18:14:29 +1300

    save wip

--- opencv-example.mk
+++ opencv-example.mk
@@ -1,11 +1,10 @@
 ################################################################################
 #
-# libfoo
+# opencv-example
 #
 ################################################################################
 
-OPENCV_EXAMPLE_VERSION = 0.1.0
-OPENCV_EXAMPLE_SOURCE = opencv_example-$(LIBFOO_VERSION).tar.gz
+OPENCV_EXAMPLE_VERSION = 0.0.2
 OPENCV_EXAMPLE_SITE = $(call github,tobig77,opencv-example,$(OPENCV_EXAMPLE_VERSION))
 OPENCV_EXAMPLE_INSTALL_STAGING = YES
 OPENCV_EXAMPLE_INSTALL_TARGET = YES
